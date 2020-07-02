07a848dea6fd45e9399ea89bdc870241714f30a7
Mac-Nikak <65832228+Mac-Nikak@users.noreply.github.com>
2020-07-02 16:09:18 +1000

    Update Circle.java

--- Java/Yakov/Circle.java
+++ Java/Yakov/Circle.java
@@ -1,5 +1,5 @@
 public class Circle {
     /* Solve 22 issue using only three variables myRadius, myFraction and Angle, if you use more than 3 variables,
-    solution will be rejected.
+    solution will be rejected, you shouldn't use ++ or +1 in loop.
      */
 }
